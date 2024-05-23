e69cba80e45757eff4eeb388dc1d7c57e955e2e7
fadrays <olubukolafadaiya1@gmail.com>
2024-05-23 08:31:14 +0100

    user reg

---
+++ resources/js/frontend/components/ExampleComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <span>Sendmone User</span>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
